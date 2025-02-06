597933c47f4eff28da964fac192834929c35b34b
Claire Liu <cliu1012@gmail.com>
2025-02-06 09:27:03 +0000

    finished

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require "sinatra"
 require "sinatra/reloader"
 
 require "binding_of_caller"
+require "better_errors"
+
 
 # Need this configuration for better_errors
 use(BetterErrors::Middleware)
@@ -9,29 +11,62 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  erb(:elephant)
+  erb(:home)
 end
 
 
 get("/rock") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @player_choice = "Rock"
+  @computer_choice = ["Rock", "Paper", "Scissors"].sample
+  @result = ""
+
+  if @player_choice == @computer_choice
+    @result = "tied"
+  elsif (@player_choice == "Rock" && @computer_choice == "Scissors") ||
+        (@player_choice == "Scissors" && @computer_choice == "Paper") ||
+        (@player_choice == "Paper" && @computer_choice == "Rock")
+    @result = "win"
+  else
+    @result = "lost"
+  end
+
+  erb(:rock)
 end
 
 
 get("/paper") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @player_choice = "Paper"
+  @computer_choice = ["Rock", "Paper", "Scissors"].sample
+  @result = ""
+
+  if @player_choice == @computer_choice
+    @result = "tied"
+  elsif (@player_choice == "Rock" && @computer_choice == "Scissors") ||
+        (@player_choice == "Scissors" && @computer_choice == "Paper") ||
+        (@player_choice == "Paper" && @computer_choice == "Rock")
+    @result = "win"
+  else
+    @result = "lost"
+  end
+
+  erb(:paper)
 end
 
 
 get("/scissors") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @player_choice = "Scissors"
+  @computer_choice = ["Rock", "Paper", "Scissors"].sample
+  @result = ""
+
+  if @player_choice == @computer_choice
+    @result = "tied"
+  elsif (@player_choice == "Rock" && @computer_choice == "Scissors") ||
+        (@player_choice == "Scissors" && @computer_choice == "Paper") ||
+        (@player_choice == "Paper" && @computer_choice == "Rock")
+    @result = "win"
+  else
+    @result = "lost"
+  end
+
+  erb(:scissors)
 end
