6a00fffbd365cff878e0a1143e9930a80b9b3edb
Claire Liu <cliu1012@gmail.com>
2025-02-06 08:48:29 +0000

    homepage

--- app.rb
+++ app.rb
@@ -1,7 +1,35 @@
 require "sinatra"
 require "sinatra/reloader"
 
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
+
 get("/") do
+  erb(:elephant)
+end
+
+
+get("/rock") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+end
+
+
+get("/paper") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
+end
+
+
+get("/scissors") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
